dc65cb1876a5b3a5ae7da60c838f8534d5848753
Marine Tissier <marine.tsr13@icloud.com>
2025-11-25 17:19:01 +0100

    Rename SecurityIT as SecurityIntegrationTest

--- src/test/java/com/nnk/springboot/security/SecurityIntegrationTest.java
+++ src/test/java/com/nnk/springboot/security/SecurityIntegrationTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
-class SecurityIT {
+class SecurityIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
